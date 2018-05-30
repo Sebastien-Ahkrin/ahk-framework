ce242b52b3b2d5ee5120009b24f9535985f8d5c2
Sebastien <sebastien48criquet@gmail.com>
2018-05-30 23:58:44 +0200

    export decorators and core

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Bot from './Bot'
+
+import Command from './decorators/Command'
+import Commands from './decorators/Commands'
+import Config from './decorators/Config'
+import Login from './decorators/Login'
+import Event from './decorators/Event'
+
+export {
+  Bot,
+
+  Command,
+  Commands,
+  Config,
+  Login,
+  Event
+}
